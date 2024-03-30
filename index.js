bcc6362749999e17cc53b82a3fa2e223b86dc14c
valerii.kondratko <valerii.kondratko@astorsoft.ru>
2024-03-30 12:38:34 +0700

    basic commands

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const bot = new Telegraf(
 	'7145260293:AAGzJMYpk48sNEWJ7NgXCzp3QMm1LshowJg'
 );
 
+bot.start(ctx => ctx.reply('Start command'));
+
+bot.help(ctx => ctx.reply('Help command'));
+
+bot.settings(ctx => ctx.reply('Settings'));
+
 bot.launch().then((res) => {
 	console.log('Started');
 }).catch((err) => console.log(err));
